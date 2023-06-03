394611ef0da64ad8fd3af9b3e6f5a1b5d69d081b
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-06-03 14:35:22 +0800

    Initial Commit

---
+++ MedicineApp/MedicineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicineAppApp.swift
+//  MedicineApp
+//
+//  Created by Claudio Tendean on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MedicineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
